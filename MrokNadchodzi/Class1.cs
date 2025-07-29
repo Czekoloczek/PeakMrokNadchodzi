a7f4775605edeaf725a860c72c631b7315ab3324
Czekoloczek <karkarczyk@gmail.com>
2025-07-29 22:22:34 +0200

    init

---
+++ MrokNadchodzi/Class1.cs
@@ -0,0 +1,98 @@
+﻿using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using HarmonyLib;
+
+[BepInPlugin("czekoloczek.peak.mroknadchodzi", "Mrok Nadchodzi", "1.0.0")]
+public class PeakMod : BaseUnityPlugin
+{
+    private AudioClip replacementClip;
+
+    private void Awake()
+    {
+        Logger.LogInfo("Mrok nadchodzi was loaded!");
+        var harmony = new Harmony("com.czekoloczek.peakmod.mroknadchodzi");
+        harmony.PatchAll();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        StartCoroutine(LoadAudio());
+    }
+
+    private IEnumerator LoadAudio()
+    {
+        string path = $"file://{Paths.PluginPath}/MrokNadchodzi/replacement.ogg";
+        using (WWW www = new WWW(path))
+        {
+            yield return www;
+            replacementClip = www.GetAudioClip(false, false, AudioType.OGGVORBIS);
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Only replace audio when scene name is Level_x
+        if (System.Text.RegularExpressions.Regex.IsMatch(scene.name, @"^Level_\d+$"))
+        {
+            StartCoroutine(ReplaceAudio());
+        }
+    }
+
+    private IEnumerator ReplaceAudio()
+    {
+        yield return new WaitForSeconds(1f);
+        var audioObj = GameObject.Find("GAME/GUIManager/Canvas_HUD/Notification/SFX");
+        if (audioObj != null && replacementClip != null)
+        {
+            var audioSource = audioObj.GetComponent<AudioSource>();
+            if (audioSource != null)
+            {
+                audioSource.clip = replacementClip;
+                Logger.LogInfo("Dźwięk został podmieniony.");
+            }
+        }
+    }
+}
+
+// Patch for LocalizedText.RefreshText
+[HarmonyPatch]
+public static class LocalizedText_RefreshText_Patch
+{
+    static System.Reflection.MethodBase TargetMethod()
+    {
+        var type = AccessTools.TypeByName("LocalizedText");
+        return AccessTools.Method(type, "RefreshText");
+    }
+
+    static void Postfix(object __instance)
+    {
+        var comp = __instance as Component;
+        if (comp != null && comp.gameObject != null)
+        {
+            string path = comp.gameObject.transform.GetHierarchyPath();
+            if (path.EndsWith("GAME/GUIManager/Canvas_HUD/Notification/Fog"))
+            {
+                var tmp = comp.GetComponent<TMPro.TextMeshProUGUI>();
+                if (tmp != null)
+                {
+                    tmp.text = "MROK NADCHODZI...";
+                }
+            }
+        }
+    }
+}
+
+// Helper do pobierania pełnej ścieżki obiektu
+public static class TransformExtensions
+{
+    public static string GetHierarchyPath(this Transform transform)
+    {
+        string path = transform.name;
+        while (transform.parent != null)
+        {
+            transform = transform.parent;
+            path = transform.name + "/" + path;
+        }
+        return path;
+    }
+    public static string GetHierarchyPath(this GameObject go) => go.transform.GetHierarchyPath();
+}
---
+++ MrokNadchodzi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("MrokNadchodzi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MrokNadchodzi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("dd95be33-0b92-4e5b-ae6e-4febf59ebf12")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
